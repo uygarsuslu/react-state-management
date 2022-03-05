619c94c5fd583d93d26553656ee668edad920a51
Uygar Süslü <uygarsuslu1@gmail.com>
2022-03-05 19:11:28 +0300

    react state management added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import React from 'react';
 import './App.css';
 import MovieList from "./MovieList";
 import Nav from "./Nav";
 import { MovieProvider } from "./MovieContext";
+import AddMovie from "./AddMovie";
 
 function App() {
   return (
     <MovieProvider>
       <div className="App">
         <Nav />
+        <AddMovie />
         <MovieList />
       </div>
     </MovieProvider>
