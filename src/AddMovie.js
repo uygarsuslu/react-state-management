523d0d0838b3468a15ee620002641bb61e1e98a8
Uygar Süslü <uygarsuslu1@gmail.com>
2022-03-05 19:10:17 +0300

    add movie added

--- src/AddMovie.js
+++ src/AddMovie.js
@@ -1,27 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { MovieContext } from "./MovieContext";
 
 const AddMovie = () => {
 
     const [name, setName] = useState("")
     const [price, setPrice] = useState("")
 
+    const [movies, setMovies] = useContext(MovieContext)
+
     const updateName = (e) => {
         setName(e.target.value)
     }
 
     const updatePrice = (e) => {
-        setName(e.target.value)
+        setPrice(e.target.value)
     }
 
-    const AddMovie = e => {
+    const addMovie = e => {
         e.preventDefault()
+        setMovies(prevMovies => [...prevMovies, { name: name, price: price }])
     }
 
     return (
-        <form>
+        <form onSubmit={addMovie}>
             <input type="text" name="name" value={name} onChange={updateName} />
+            <br />
             <input type="text" name="price" value={price} onChange={updatePrice} />
+            <br />
             <button>Submit</button>
         </form>
     )
 }
+
+export default AddMovie;
